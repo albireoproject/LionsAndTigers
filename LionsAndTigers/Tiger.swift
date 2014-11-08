e894e984e9110c0b82050707ae43d78d6a19ddfe
Hongsun Yoon <albireo@Albireos-DevBook.local>
2014-11-08 23:16:37 +0900

    first commit

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,17 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Hongsun Yoon, M.D. on 11/4/14.
+//  Copyright (c) 2014 Hongsun Yoon, M.D. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named:"")
+}
